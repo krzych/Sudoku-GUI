7d66e7bb2ada500b1c03b18d4f76931145d52994
Krzysztof <sas.krzysztof@gmail.com>
2012-09-30 22:39:17 +0200

    Basic camera acquisition code added

--- Sudoku/core/ImageAcquirer.cpp
+++ Sudoku/core/ImageAcquirer.cpp
@@ -16,5 +16,21 @@ ImageAcquirer::~ImageAcquirer(void)
 
 cv::Mat ImageAcquirer::AcquireImage(const char* file_name)
 {
-	return cv::imread(file_name, CV_LOAD_IMAGE_GRAYSCALE);
+	cvNamedWindow("Test",CV_WINDOW_AUTOSIZE);
+	CvCapture *capture = cvCreateCameraCapture(0);
+	IplImage *frame;
+	cv::Mat matFrame;
+	while(cv::waitKey(33) != 27)
+	{
+		frame = cvQueryFrame(capture);
+		matFrame = cv::Mat(frame,true);
+		if(!frame) break;
+		cvShowImage("Test",frame);
+	}
+
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Test");
+
+
+	return matFrame;
 }
