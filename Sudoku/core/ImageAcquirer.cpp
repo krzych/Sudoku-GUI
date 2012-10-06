1f980f84bee5ce3d7a4defb429d413cc72bb46bf
Krzysztof <sas.krzysztof@gmail.com>
2012-10-06 18:46:42 +0200

    Android Camera with IP Webcam

--- Sudoku/core/ImageAcquirer.cpp
+++ Sudoku/core/ImageAcquirer.cpp
@@ -2,8 +2,18 @@
 #include "ImageAcquirer.h"
 
 #include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
 #include <string>
 #include <iostream>
+
+#define ANDROIDCAM
+
+#ifdef ANDROIDCAM
+#define SHOTURL "http://192.168.1.104:8080/shot.jpg"
+#define TEMPJPG "D:/shot.jpg"
+#include <UrlMon.h>
+#pragma comment(lib,"urlmon.lib")
+#endif
 
 ImageAcquirer::ImageAcquirer(void)
 {
@@ -16,21 +26,33 @@ ImageAcquirer::~ImageAcquirer(void)
 
 cv::Mat ImageAcquirer::AcquireImage(const char* file_name)
 {
-	cvNamedWindow("Test",CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("Video",CV_WINDOW_AUTOSIZE);
+	cv::Mat matFrame;
+#ifdef ANDROIDCAM
+	while(cv::waitKey(5) != 27)
+	{
+		HRESULT hr = URLDownloadToFile(NULL, _T(SHOTURL),
+			_T(TEMPJPG),0,NULL);
+		IplImage *frame = cvLoadImage(TEMPJPG);
+		matFrame = cv::Mat(frame,true);
+		cvShowImage("Video",frame);
+	}
+#else
 	CvCapture *capture = cvCreateCameraCapture(0);
 	IplImage *frame;
-	cv::Mat matFrame;
+	
 	while(cv::waitKey(33) != 27)
 	{
 		frame = cvQueryFrame(capture);
 		matFrame = cv::Mat(frame,true);
 		if(!frame) break;
-		cvShowImage("Test",frame);
+		cvShowImage("Video",frame);
 	}
 
 	cvReleaseCapture(&capture);
-	cvDestroyWindow("Test");
-
+#endif
+	cv::cvtColor(matFrame,matFrame,CV_BGR2GRAY);
+	cvDestroyWindow("Video");
 
 	return matFrame;
 }
